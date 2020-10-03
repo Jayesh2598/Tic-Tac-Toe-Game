6b0aa2ae7f820b95ccb6b973796a7f9789507fdf
Jay002 <jayesh2598@gmail.com>
2020-10-03 15:47:55 +0530

    Added: Functionality to check an index is added

--- com/capgemini/tictactoe/TicTacToeGame.java
+++ com/capgemini/tictactoe/TicTacToeGame.java
@@ -6,17 +6,18 @@ public class TicTacToeGame {
 
 	private static final Scanner SC = new Scanner(System.in);
 	public static char[] board=new char[10];
+	public static char player;
+	public static char computer;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game!");
 		board = createBoard();
-		char player = chooseLetter();
-		char computer = 'x';
-		if (player == 'x')
-			computer = 'o';
+		player = chooseLetter();
+		computer = (player == 'o')?'x':'o';
 		System.out.println("Player letter is '" + player + "' and computer letter is '" + computer + "'");
 		displayBoard(board);
-
+		System.out.println(isFree(board, playerIndex()));
+		
 	}
 
 	// creating empty board character array
@@ -49,5 +50,26 @@ public class TicTacToeGame {
 		System.out.println("--------------");
 		System.out.println(board[7] +"  |  "+ board[8]+"  |  "+ board[9]);
 		
+	}
+	
+	//Player makes a move
+	private static int playerIndex() {
+		int index=0;
+		while (true) {
+			System.out.println("Enter the desired location index:");
+			int input = SC.nextInt();
+			if(input>=1 && input<=9) {
+				index=input;
+				break;
+			}	
+			else
+				System.out.println("Enter number between 1-9");
+		}
+		return index;
+	}
+	
+	//Checking if index position is free
+	private static boolean isFree(char[] board, int index) {
+		return board[index]=='\0';
 	}
 }
