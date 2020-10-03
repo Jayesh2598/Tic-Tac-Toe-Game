461610f496c0f4e80c0727126ce5f59aa8dccb73
Jay002 <jayesh2598@gmail.com>
2020-10-03 14:20:27 +0530

    Added: The computer and player letters are set

--- com/capgemini/tictactoe/TicTacToeGame.java
+++ com/capgemini/tictactoe/TicTacToeGame.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	
+	static Scanner sc=new Scanner(System.in);
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game!");
 		char[] board= createBoard();
+		char player=chooseLetter();
+		char computer='x';
+		if(player=='x')
+			computer='o';
+		System.out.println("Player letter is '"+player+"' and computer letter is '"+computer+"'");
 	}
 	
 	//creating empty board character array
@@ -15,5 +22,21 @@ public class TicTacToeGame {
 		for(int index=0;index<10;index++)
 			board[index]='\0';
 		return board;
+	}
+	
+	//Player choosing a letter
+	private static char chooseLetter() {
+		char player='\0';
+		while(true) {
+			System.out.println("Choose a letter (x or o):");
+			char input = sc.next().toLowerCase().charAt(0);
+			if(input=='x' || input=='o' ) {
+				player=input;
+				break;
+			}	
+			else
+				System.out.println("You can only choose between x and o.");	
+		}
+		return player;
 	}
 }
