dd77c5e9bbb7e4b1aaa1ad3c75e976838dcd480e
Jay002 <jayesh2598@gmail.com>
2020-10-03 14:05:09 +0530

    Added: Array of game board is initialized

--- com/capgemini/tictactoe/TicTacToeGame.java
+++ com/capgemini/tictactoe/TicTacToeGame.java
@@ -1,8 +1,19 @@
 package com.capgemini.tictactoe;
 
-public class TicTacToeGame {
+import java.util.Scanner;
 
+public class TicTacToeGame {
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game!");
+		char[] board= createBoard();
+	}
+	
+	//creating empty board character array
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for(int index=0;index<10;index++)
+			board[index]='\0';
+		return board;
 	}
 }
