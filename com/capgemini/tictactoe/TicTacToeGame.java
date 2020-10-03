4d39a9bc527c164f38fa9ab06b1760e100409a1c
Jay002 <jayesh2598@gmail.com>
2020-10-03 15:06:51 +0530

    Added: method to display the board is added

--- com/capgemini/tictactoe/TicTacToeGame.java
+++ com/capgemini/tictactoe/TicTacToeGame.java
@@ -3,40 +3,51 @@ package com.capgemini.tictactoe;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	
-	static Scanner sc=new Scanner(System.in);
 
+	private static final Scanner SC = new Scanner(System.in);
+	public static char[] board=new char[10];
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game!");
-		char[] board= createBoard();
-		char player=chooseLetter();
-		char computer='x';
-		if(player=='x')
-			computer='o';
-		System.out.println("Player letter is '"+player+"' and computer letter is '"+computer+"'");
+		board = createBoard();
+		char player = chooseLetter();
+		char computer = 'x';
+		if (player == 'x')
+			computer = 'o';
+		System.out.println("Player letter is '" + player + "' and computer letter is '" + computer + "'");
+		displayBoard(board);
+
 	}
-	
-	//creating empty board character array
+
+	// creating empty board character array
 	private static char[] createBoard() {
-		char[] board = new char[10];
-		for(int index=0;index<10;index++)
-			board[index]='\0';
+		for (int index = 0; index < board.length; index++)
+			board[index] = '\0';
 		return board;
 	}
-	
-	//Player choosing a letter
+
+	// Player choosing a letter
 	private static char chooseLetter() {
-		char player='\0';
-		while(true) {
-			System.out.println("Choose a letter (x or o):");
-			char input = sc.next().toLowerCase().charAt(0);
-			if(input=='x' || input=='o' ) {
-				player=input;
+		char player = '\0';
+		while (true) {
+			System.out.println("Choose player letter (x or o):");
+			char input = SC.next().toLowerCase().charAt(0);
+			if (input == 'x' || input == 'o') {
+				player = input;
 				break;
-			}	
-			else
-				System.out.println("You can only choose between x and o.");	
+			} else
+				System.out.println("You can only choose between x and o.");
 		}
 		return player;
+	}
+
+	// Displaying board
+	private static void displayBoard(char[] board) {
+		System.out.println("\n" + board[1] +"  |  "+ board[2]+"  |  "+ board[3]);
+		System.out.println("--------------");
+		System.out.println(board[4] +"  |  "+ board[5]+"  |  "+ board[6]);
+		System.out.println("--------------");
+		System.out.println(board[7] +"  |  "+ board[8]+"  |  "+ board[9]);
+		
 	}
 }
